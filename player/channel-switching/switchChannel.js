61be991272dc1f2799493fa676fda992f48e4b51
lukaskroepfl <lukas.kroepfl@gmail.com>
2018-05-24 14:29:16 +0200

    added hls streams to 2 channels of channel switching demo

--- player/channel-switching/switchChannel.js
+++ player/channel-switching/switchChannel.js
@@ -2,7 +2,8 @@ function switchChannel(channelID) {
   var source;
   if (channelID === '1') {
     source = {
-      dash: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd'
+      dash: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+      hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
     }
   } else if (channelID === '2') {
     source = {
@@ -10,7 +11,8 @@ function switchChannel(channelID) {
     };
   } else {
     source = {
-      dash: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd'
+      dash: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd',
+      hls: 'https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
     }
   }
 
