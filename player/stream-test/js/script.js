1d5be89d3d2e15e388f1f57a87d5d1066fc50581
Valentino Zablocki <valentinozablocki2@gmail.com>
2018-06-20 09:57:52 +0200

    Removed comments

--- player/stream-test/js/script.js
+++ player/stream-test/js/script.js
@@ -104,16 +104,10 @@ function checkIsUrlValid(url, urlPurpose) {
 
 function getParamsQueryString() {
   var key = false, res = {}, itm = null;
-  // get the query string without the ?
   var qs = location.search.substring(1);
-  // check for the key as an argument
   if (arguments.length > 0 && arguments[0].length > 1)
     key = arguments[0];
-  // make a regex pattern to grab key/value
   var pattern = /([^&=]+)=([^&]*)/g;
-  // loop the items in the query string, either
-  // find a match to the argument, or build an object
-  // with key/value pairs
   while (itm = pattern.exec(qs)) {
     if (key !== false && decodeURIComponent(itm[1]) === key)
       return decodeURIComponent(itm[2]);
