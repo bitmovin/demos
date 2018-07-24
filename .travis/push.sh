b5c3717f701b14df352d3d054a52c473b7d586dc
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2018-07-24 13:05:59 +0200

    Added travis after success

---
+++ .travis/push.sh
@@ -0,0 +1,14 @@
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git push
+}
+
+setup_git
+commit_website_files
