85c8fd053bfbab83bacf37308a012e6173c5134d
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2018-10-11 11:51:09 +0200

    Updated smooth source to https

--- player/stream-test/js/script.js
+++ player/stream-test/js/script.js
@@ -45,7 +45,7 @@ var config = {
     dash: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
     hls: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
     progressive: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
-    smooth: 'http://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest',
+    smooth: 'https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest',
     poster: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
   },
   drmSource: {
