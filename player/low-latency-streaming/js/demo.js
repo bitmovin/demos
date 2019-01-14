af38198a0b1fa9ea7d39490345eea09da2d0aa12
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2019-01-14 15:30:31 +0100

    Small code changes

--- player/low-latency-streaming/js/demo.js
+++ player/low-latency-streaming/js/demo.js
@@ -14,112 +14,114 @@ var dashUrl = 'https://akamaibroadcasteruseast.akamaized.net/cmaf/live/657078/ak
 
 var url = new URL(location.href);
 var targetLatencyFromUrl = url.searchParams.get('latency');
+
 if (targetLatencyFromUrl && !isNaN(Number(targetLatencyFromUrl))) {
-targetLatency = targetLatencyFromUrl;
+    targetLatency = targetLatencyFromUrl;
 }
 
 if (url.searchParams.get('videoOnly')) {
-videoOnly = Boolean(url.searchParams.get('videoOnly'));
+    videoOnly = Boolean(url.searchParams.get('videoOnly'));
 }
 
 if (url.searchParams.get('dashUrl')) {
-dashUrl = url.searchParams.get('dashUrl');
+    dashUrl = url.searchParams.get('dashUrl');
 }
 
 var updateTargetLatency = function() {
-targetLatencyDisplay.innerText = slider.value + 's';
-targetLatency = Number(slider.value);
-player.lowlatency.setTargetLatency(targetLatency);
+    targetLatencyDisplay.innerText = slider.value + 's';
+    targetLatency = Number(slider.value);
+    player.lowlatency.setTargetLatency(targetLatency);
 };
+
 slider.oninput = updateTargetLatency;
 slider.value = String(targetLatency);
 
 var conf = {
-key: '89f6ed6c-ab0e-46c2-ac47-5665e60c3c41',
-playback: {
-    autoplay: true,
-    muted: true,
-},
-adaptation: {
-    preload: false,
-},
-logs: {
-    //level: 'debug'
-},
-style: {},
-events: {
-    playbackspeedchanged: function(e) {
-    playbackSpeedDisplay.innerText = e.to;
+    key: '89f6ed6c-ab0e-46c2-ac47-5665e60c3c41',
+    playback: {
+        autoplay: true,
+        muted: true,
     },
-},
-tweaks: {
-    RESTART_THRESHOLD: 0.2,
-    RESTART_THRESHOLD_DELTA: 0.05,
-    STARTUP_THRESHOLD: 0.2,
-    STARTUP_THRESHOLD_DELTA: 0.05,
-    END_OF_BUFFER_TOLERANCE: 0.05,
-    LIVE_EDGE_DISTANCE: 0.5,
-    LOW_LATENCY_BUFFER_GUARD: 0.8,
-    CHUNKED_CMAF_STREAMING: true,
-},
-live: {
-    lowLatency: {
-    targetLatency: 3,
-    catchup: {
-        playbackRateThreshold: 0.075,
-        seekThreshold: 5,
-        playbackRate: 1.2,
+    adaptation: {
+        preload: false,
     },
-    fallback: {
-        playbackRateThreshold: 0.075,
-        seekThreshold: 5,
-        playbackRate: 0.95,
+    logs: {
+        //level: 'debug'
     },
+    style: {},
+    events: {
+        playbackspeedchanged: function(e) {
+        playbackSpeedDisplay.innerText = e.to;
+        },
     },
-},
-nonetwork: {
-    preprocessHttpResponse: function(type, response) {
-    if (type === 'manifest/dash') {
-        if (videoOnly) {
-        response.body = response.body.replace('mp4a.40.2', 'dummy');
+    tweaks: {
+        RESTART_THRESHOLD: 0.2,
+        RESTART_THRESHOLD_DELTA: 0.05,
+        STARTUP_THRESHOLD: 0.2,
+        STARTUP_THRESHOLD_DELTA: 0.05,
+        END_OF_BUFFER_TOLERANCE: 0.05,
+        LIVE_EDGE_DISTANCE: 0.5,
+        LOW_LATENCY_BUFFER_GUARD: 0.8,
+        CHUNKED_CMAF_STREAMING: true,
+    },
+    live: {
+        lowLatency: {
+        targetLatency: 3,
+        catchup: {
+            playbackRateThreshold: 0.075,
+            seekThreshold: 5,
+            playbackRate: 1.2,
+        },
+        fallback: {
+            playbackRateThreshold: 0.075,
+            seekThreshold: 5,
+            playbackRate: 0.95,
+        },
+        },
+    },
+    nonetwork: {
+        preprocessHttpResponse: function(type, response) {
+        if (type === 'manifest/dash') {
+            if (videoOnly) {
+            response.body = response.body.replace('mp4a.40.2', 'dummy');
+            }
+        }
+        return response;
         }
     }
-    return response;
-    }
-}
 };
 
 var source = { dash: dashUrl };
 
 function printBufferLevels() {
-var videoBuffer = Math.round(player.getVideoBufferLength() * 100) / 100;
-var audioBuffer = Math.round(player.getAudioBufferLength() * 100) / 100;
+    var videoBuffer = Math.round(player.getVideoBufferLength() * 100) / 100;
+    var audioBuffer = Math.round(player.getAudioBufferLength() * 100) / 100;
 
-if (videoBuffer !== null) {
-    videoBufferDisplay.innerText = videoBuffer + 's';
-}
+    if (videoBuffer !== null) {
+        videoBufferDisplay.innerText = videoBuffer + 's';
+    }
 
-if (audioBuffer !== null) {
-    audioBufferDisplay.innerText = audioBuffer + 's';
-}
+    if (audioBuffer !== null) {
+        audioBufferDisplay.innerText = audioBuffer + 's';
+    }
 }
 
 function loadPlayer() {
-player = new bitmovin.player.Player(document.getElementById('player-container'), conf);
-player.load(source).then(function() {
-// ABR is not supported yet for low latency
-player.setVideoQuality(player.getAvailableVideoQualities()[0].id);
-updateTargetLatency();
+    player = new bitmovin.player.Player(document.getElementById('player-container'), conf);
+    player.load(source).then(function() {
+    // ABR is not supported yet for low latency
+    player.setVideoQuality(player.getAvailableVideoQualities()[0].id);
+    updateTargetLatency();
 
-setInterval(function() {
-    if (player && !player.isPaused()) {
-    var currentLatency = player.lowlatency.getLatency();
-    latencyDisplay.innerText = Math.round(currentLatency * 1000) / 1000 + 's';
-    timeDisplay.innerText = (new Date()).toISOString().substr(11,10);
-    printBufferLevels();
-    }
-}, 50);
-});
+    setInterval(function() {
+        if (player && !player.isPaused()) {
+        var currentLatency = player.lowlatency.getLatency();
+        latencyDisplay.innerText = Math.round(currentLatency * 1000) / 1000 + 's';
+        timeDisplay.innerText = (new Date()).toISOString().substr(11,10);
+        printBufferLevels();
+        }
+    }, 50);
+    });
 }
 
 $(document).ready(loadPlayer);
