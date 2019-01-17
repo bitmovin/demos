11fae871550a50375624e5038fe31772956b3b58
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2019-01-17 10:12:01 +0100

    Added analytics prefix

--- player/preload-vod/js/script.js
+++ player/preload-vod/js/script.js
@@ -4,7 +4,7 @@ var preloadConf = {
   key: '29ba4a30-8b5e-4336-a7dd-c94ff3b25f30',
   analytics: {
     key: '45adcf9b-8f7c-4e28-91c5-50ba3d442cd4',
-    videoId: 'preload-vod'
+    videoId: 'preload-vod-preloaded'
   },
   adaptation: {
     desktop: {
