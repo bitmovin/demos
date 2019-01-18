0a9f54716e518dc3f558213b8a33979613c60198
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2019-01-18 10:47:10 +0100

    Added 4k content

--- player/4k/js/script.js
+++ player/4k/js/script.js
@@ -1,4 +1,4 @@
-var pageLoadedTime;
+var playTimestamp;
 
 var conf = {
   key: '29ba4a30-8b5e-4336-a7dd-c94ff3b25f30',
@@ -6,14 +6,21 @@ var conf = {
     key: '45adcf9b-8f7c-4e28-91c5-50ba3d442cd4',
     videoId: '4k'
   },
+  events: {
+    play: function(e) {
+      playTimestamp = e.timestamp;
+    },
+    playing: function(e) {
+      document.getElementById('startup').innerHTML = e.timestamp - playTimestamp + 'ms';
+    }
+  },
   playback: {
     muted: true
   }
 };
 
 var source = {
-  dash: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd',
-  hls: 'https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+  hls: 'http://cdn.bitmovin.com/content/demos/4k/38e843e0-1998-11e9-8a92-c734cd79b4dc/manifest.m3u8',
   poster: 'https://bitmovin-a.akamaihd.net/content/sintel/poster.png'
 };
 
@@ -21,12 +28,11 @@ var playerContainer = document.getElementById('player-container');
 var player = new bitmovin.player.Player(playerContainer, conf);
 
 function loadPlayer() {
-  player.load(source).then(function() {
-    document.getElementById('startup').innerHTML = Date.now() - pageLoadedTime + 'ms';
+  player.load(source).then(() => {
+    player.setVideoQuality('1600_50128000');
   });
 }
 
 $(document).ready(function() {
-  pageLoadedTime = Date.now();
   loadPlayer();
 });
