88846c8b71dd6bfe1059b90f26d524b039ebe8c2
Valentino Zablocki <tino@Valentinos-MacBook-Pro.local>
2019-01-31 14:15:37 +0100

    Fixed low-latency for IE

--- player/low-latency-streaming/js/script.js
+++ player/low-latency-streaming/js/script.js
@@ -12,8 +12,9 @@ var targetLatency = 3;
 var videoOnly = false;
 var dashUrl = 'https://akamaibroadcasteruseast.akamaized.net/cmaf/live/657078/akasource/out.mpd';
 
-var url = new URL(location.href);
-var targetLatencyFromUrl = url.searchParams.get('latency');
+var queryString = getQueryString();
+
+var targetLatencyFromUrl = queryString.latency;
 
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
@@ -21,12 +22,12 @@ if (targetLatencyFromUrl && !isNaN(Number(targetLatencyFromUrl))) {
     targetLatency = targetLatencyFromUrl;
 }
 
-if (url.searchParams.get('videoOnly') && url.searchParams.get('videoOnly') !== 'false') {
+if (queryString.videoOnly && queryString.videoOnly !== 'false') {
     videoOnly = true;
 }
 
-if (url.searchParams.get('dashUrl')) {
-    dashUrl = url.searchParams.get('dashUrl');
+if (queryString.dashUrl) {
+    dashUrl = queryString.dashUrl;
 }
 
 var updateTargetLatency = function() {
@@ -135,6 +136,28 @@ function loadPlayer() {
         }
     }, 50);
     });
+}
+
+function getQueryString() {
+    var key = false, res = {}, itm = null;
+    // get the query string without the ?
+    var qs = location.search.substring(1);
+    // check for the key as an argument
+    if (arguments.length > 0 && arguments[0].length > 1)
+      key = arguments[0];
+    // make a regex pattern to grab key/value
+    var pattern = /([^&=]+)=([^&]*)/g;
+    // loop the items in the query string, either
+    // find a match to the argument, or build an object
+    // with key/value pairs
+    while (itm = pattern.exec(qs)) {
+      if (key !== false && decodeURIComponent(itm[1]) === key)
+        return decodeURIComponent(itm[2]);
+      else if (key === false)
+        res[decodeURIComponent(itm[1])] = decodeURIComponent(itm[2]);
+    }
+
+    return key === false ? res : null;
 }
 
 $(document).ready(loadPlayer);
