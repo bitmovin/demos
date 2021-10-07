7c2c3bcb338b5f400f7e967dabfa79581e45d4e2
Wolfram Hofmeister <wolfram.hofmeister@bitmovin.com>
2021-10-07 19:53:01 +0200

    Update the source shown on the demo page as well

--- player/vr-360/demo.js
+++ player/vr-360/demo.js
@@ -9,9 +9,9 @@ var source = {
   dash:
     'https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd',
   hls:
-    'https://bitmovin.com/player-content/playhouse-vr/m3u8s/105560.m3u8',
+    'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8',
   progressive:
-    'https://bitmovin.com/player-content/playhouse-vr/progressive.mp4',
+    'https://bitmovin-a.akamaihd.net/content/playhouse-vr/progressive.mp4',
   poster:
     'https://bitmovin-a.akamaihd.net/content/playhouse-vr/poster.jpg',
   vr: {
