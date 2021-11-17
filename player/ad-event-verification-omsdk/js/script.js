f71d52208dfde9d3c93bb7c1902582414189503e
Jameson Steiner <jameson.steiner@gmail.com>
2021-11-17 18:53:40 +0100

    Change template string to string concatenation

--- player/ad-event-verification-omsdk/js/script.js
+++ player/ad-event-verification-omsdk/js/script.js
@@ -1,3 +1,4 @@
+var adTag = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=' + Date.now();
 var conf = {
     key: '29ba4a30-8b5e-4336-a7dd-c94ff3b25f30',
     playback: {
@@ -6,7 +7,7 @@ var conf = {
     advertising: {
         adBreaks: [{
             tag: {
-                url: `https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=${Date.now()}`, // TODO:
+                url: adTag,
                 type: 'vast'
             },
             position: 'pre',
