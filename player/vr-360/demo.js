c7663d5109ea16ce8d93a114a1eff93daf2c9865
Lukas Kröpfl <lukas.kroepfl@protonmail.com>
2022-08-24 15:09:32 +0200

    fixed missing ,

--- player/vr-360/demo.js
+++ player/vr-360/demo.js
@@ -9,7 +9,7 @@ var source = {
   dash: 'https://cdn.bitmovin.com/content/assets/playhouse-vr/mpds/105560.mpd',
   hls: 'https://cdn.bitmovin.com/content/assets/playhouse-vr/m3u8s/105560.m3u8',
   progressive: 'https://cdn.bitmovin.com/content/assets/playhouse-vr/progressive.mp4',
-  poster: 'https://cdn.bitmovin.com/content/assets/playhouse-vr/poster.jpg'
+  poster: 'https://cdn.bitmovin.com/content/assets/playhouse-vr/poster.jpg',
   vr: {
     startupMode: '2d',
     startPosition: 180,
