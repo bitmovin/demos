58feff82dd1bb5af2fc33c8c47fc76810b61a02c
Andreas Kogler <andy.kogler.junior@gmail.com>
2025-08-29 11:16:27 +0200

    Remove superfluous SSAI demo remnants

--- player/sgai/js/script.js
+++ player/sgai/js/script.js
@@ -1,9 +1,8 @@
 var conf = {
   key: '29ba4a30-8b5e-4336-a7dd-c94ff3b25f30',
-  // key: '89f6ed6c-ab0e-46c2-ac47-5665e60c3c41',
   analytics: {
     key: '45adcf9b-8f7c-4e28-91c5-50ba3d442cd4',
-    videoId: 'server-side-ad-insertion',
+    videoId: 'server-guided-ad-insertion',
   },
   logs: {
     level: 'debug',
@@ -32,23 +31,7 @@ var playerContainer = document.getElementById('player-container');
 bitmovin.player.Player.addModule(bitmovin.analytics.PlayerModule);
 var player = new bitmovin.player.Player(playerContainer, conf);
 
-player.load(source).then(function (value) {
-  player.on(bitmovin.player.PlayerEvent.TimeChanged, function (data) {
-    if (player.getCurrentTime() >= 29.081333 && player.getCurrentTime() < 41.138666) {
-      document.getElementsByClassName('bmpui-ui-container bmpui-controlbar-top')[0].style.display = 'none';
-      hidden = true;
-      played = true;
-    } else if (hidden) {
-      document.getElementsByClassName('bmpui-ui-container bmpui-controlbar-top')[0].style.display = 'block';
-    }
-  });
-  player.on(bitmovin.player.PlayerEvent.Seek, function (data) {
-    if (data.seekTarget > 29 && !played) {
-      player.seek(29);
-      played = true;
-    }
-  });
-});
+player.load(source);
 
 (function () {
   if (isAdblockEnabled) {
