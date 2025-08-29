0ff2273974f59f1bdfc0404a4b5a82fea0773341
Andreas Kogler <andy.kogler.junior@gmail.com>
2025-08-29 13:42:43 +0200

    Add `network` config for `EXT-X-DEFINE` processing

--- player/sgai/js/script.js
+++ player/sgai/js/script.js
@@ -1,3 +1,5 @@
+var url, sessionId;
+
 var conf = {
   key: '29ba4a30-8b5e-4336-a7dd-c94ff3b25f30',
   analytics: {
@@ -12,6 +14,41 @@ var conf = {
   },
   playback: {
     muted: true,
+  },
+  network: {
+    preprocessHttpResponse: (type, response) => {
+      if (type === 'manifest/hls/master') {
+        const lines = response.body.split('\n');
+
+        const hostRegex = /^#EXT-X-DEFINE:NAME="host",VALUE="([^"]+)"/;
+        const sessionIdRegex = /^#EXT-X-DEFINE:NAME="sessionId",VALUE="([^"]+)"/;
+
+        lines.forEach((line) => {
+          const trimmedLine = line.trim();
+
+          const hostMatch = trimmedLine.match(hostRegex);
+          if (hostMatch) {
+            url = hostMatch[1];
+          }
+
+          const sessionMatch = trimmedLine.match(sessionIdRegex);
+          if (sessionMatch) {
+            sessionId = sessionMatch[1];
+          }
+        });
+
+        console.warn('YOLO-> Extracted URL', url);
+        console.warn('YOLO-> Extracted session ID', sessionId);
+      }
+
+      if (type === 'manifest/hls/variant') {
+        console.warn('Replacing DEFINE tags in variant');
+        response.body = response.body.replaceAll('{$host}', url);
+        response.body = response.body.replaceAll('{$sessionId}', sessionId);
+      }
+
+      return Promise.resolve(response);
+    },
   },
   location: {
     ui: 'https://cdn.bitmovin.com/player/web/8/bitmovinplayer-ui.js',
