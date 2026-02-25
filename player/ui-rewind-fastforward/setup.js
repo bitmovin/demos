95cd6471b7e343b14af2139f37dc2e981455f139
Michele Pozzi <123.mpozzi@gmail.com>
2026-02-25 18:50:32 +0100

    Update wording and switch to const

--- player/ui-rewind-fastforward/setup.js
+++ player/ui-rewind-fastforward/setup.js
@@ -3,14 +3,14 @@ const conf = {
   ui: false
 };
 
-var source = {
+const source = {
   dash: 'https://cdn.bitmovin.com/content/assets/sintel/sintel.mpd',
   hls: 'https://cdn.bitmovin.com/content/assets/sintel/hls/playlist.m3u8',
   poster: 'https://cdn.bitmovin.com/content/assets/sintel/poster.png',
 };
 
-var player = new bitmovin.player.Player(document.getElementById('player-container'), conf);
-var uiManager = new bitmovin.playerui.UIManager(player, [{
+const player = new bitmovin.player.Player(document.getElementById('player-container'), conf);
+const uiManager = new bitmovin.playerui.UIManager(player, [{
   ui: buildUI(),
 }]);
 
